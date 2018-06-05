396302de17c20648ccfbb6584df6cef11838af47
18bmartin <18bmartin@mlmc.vic.edu.au>
2018-06-06 09:30:50 +1000

    Creating tests for invalid inputs

--- SATcode.py
+++ SATcode.py
@@ -9,19 +9,19 @@ class PasswordTester:
         self.titleText = Label(master, text="Password Tester")
         self.titleText.grid()
 
-        self.inputText1 = Label(master, text="Enter Password")
+        self.inputText1 = Label(master, text="              Enter Password   ")
         self.inputText1.grid(row = 1, column = 0)
 
-        self.pass1 = DoubleVar()
-        self.entrypass1 = Entry(master, textvariable=self.pass1)
-        self.entrypass1.grid(row = 1, column = 1)
+        self.pass1 = StringVar()
+        self.enterPass1 = Entry(master, textvariable=self.pass1)
+        self.enterPass1.grid(row = 1, column = 1)
 
-        self.inputText2 = Label(master, text="Re-Enter Password")
+        self.inputText2 = Label(master, text="           Re-Enter Password      ")
         self.inputText2.grid(row = 2, column = 0)
 
-        self.pass2 = DoubleVar()
-        self.entrypass2 = Entry(master, textvariable=self.pass2)
-        self.entrypass2.grid(row = 2, column = 1)
+        self.pass2 = StringVar()
+        self.enterPass2 = Entry(master, textvariable=self.pass2)
+        self.enterPass2.grid(row = 2, column = 1)
 
         self.TestButton = Button(master, text="Test", command=self.Test)
         self.TestButton.grid(row=4, column=0)
@@ -29,8 +29,29 @@ class PasswordTester:
         self.ExitButton = Button(master, text="Exit", command=master.quit)
         self.ExitButton.grid(row=4, column=1)
 
+        self.spaceText = Label(master, text='''                          
+        
+        
+        ''')
+        self.spaceText.grid(row = 5, column = 0)
+
     def Test(self):
-        x = 0
+        p1 = self.pass1.get()
+        p2 = self.pass2.get()
+        length = len(p1)
+
+        if length < 8:
+            self.errorText = Label(text= '''Your password must 
+    be greater than or equal 
+    to 8 characters in length ''')
+            self.errorText.grid(row=5)
+        elif p1 != p2:
+            self.errorText = Label(text='''The 2 passwords you
+    have entered do not match
+                                    ''')
+            self.errorText.grid(row=5)
+        else:
+            x = 0
 
 root = Tk()
 gui = PasswordTester(root)
