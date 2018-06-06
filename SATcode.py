76db44e2b076146bcd937733e3ef7c5564d835be
18bmartin <18bmartin@mlmc.vic.edu.au>
2018-06-06 10:27:55 +1000

    Created and split the hash and masked the user inputs.

--- SATcode.py
+++ SATcode.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import hashlib
+
+PPR = open('Pwnedpasswordresults.txt', 'a')
 
 class PasswordTester:
 
@@ -9,18 +12,18 @@ class PasswordTester:
         self.titleText = Label(master, text="Password Tester")
         self.titleText.grid()
 
-        self.inputText1 = Label(master, text="              Enter Password   ")
+        self.inputText1 = Label(master, text= "              Enter Password   ", )
         self.inputText1.grid(row = 1, column = 0)
 
         self.pass1 = StringVar()
-        self.enterPass1 = Entry(master, textvariable=self.pass1)
+        self.enterPass1 = Entry(master, show="*", textvariable=self.pass1)
         self.enterPass1.grid(row = 1, column = 1)
 
         self.inputText2 = Label(master, text="           Re-Enter Password      ")
         self.inputText2.grid(row = 2, column = 0)
 
         self.pass2 = StringVar()
-        self.enterPass2 = Entry(master, textvariable=self.pass2)
+        self.enterPass2 = Entry(master, show="*", textvariable=self.pass2)
         self.enterPass2.grid(row = 2, column = 1)
 
         self.TestButton = Button(master, text="Test", command=self.Test)
@@ -51,7 +54,12 @@ class PasswordTester:
                                     ''')
             self.errorText.grid(row=5)
         else:
-            x = 0
+            p1 = str(p1).encode('utf-8')
+            hashPassword = hashlib.sha1(p1)
+            hashPassword = hashPassword.hexdigest()
+            hPrefix = hashPassword[0:5]
+            hSuffix = hashPassword[5:]
+PPR.close()
 
 root = Tk()
 gui = PasswordTester(root)
