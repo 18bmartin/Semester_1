e481a4caa2ab041fe3442bb4ea64d91f3619a4d2
18bmartin <18bmartin@mlmc.vic.edu.au>
2018-08-27 11:40:29 +1000

    Added a binary search algorithm and commented it, changed the way the code searches for the hash suffix.

--- SATcode.py
+++ SATcode.py
@@ -1,10 +1,56 @@
+""" Author: Brent martin
+Date:
+Version: 1.0
+This code will allow users to test their passwords against a database of stolen passwords
+in order to test whether their passwords are secure. The database can either be in the form of
+the website 'PWNED passwords' with a database of stolen passwords or an offline version the user provides."""
+
 #importing the Tkinter library in order to create the GUI
 from tkinter import *
 #importing the hashlib library in order to hash the password
 import hashlib
 #importing the os library to connect to the internet website
 import os
-
+# a binary search upon the list made
+def BinarySearch(list, target):
+#defining a binary result of whether or not the target is within the list
+    Pwned = 0
+# defining the size of the list
+    low = 0
+    high = len(list)
+# while the lowest value is smaller than the highest value (when they are the same the value is not within the list)
+    while low < high:
+# finding the midpoint of the section being searching
+        mid = (low + high) // 2
+# getitng the value from the midpoint
+        val = list[mid]
+# making all the variables strings for consistent < > = comparisons
+        target = str(target)
+        val = str(val)
+        mid = str(mid)
+        low = str(low)
+        high = str(high)
+# if the target is found
+        if target == val:
+#making the binary 1 as a 'True' value to whether or not the hash Suffix is within the list
+            Pwned = 1
+#returning the position of the hash and whether or not it is within the list
+            return (mid, Pwned)
+            break
+# if the target is greater than the current value
+        elif str(target) > str(val):
+# if the lowest value is already equal to the middle value the target is not within the list
+            if low == mid:
+                break
+# make the midpoint the lowest value, allowing the code to create a new midpoint in the next loop
+            low = mid
+# if target is less than the current value
+        elif str(target) < str(val):
+# make the midpoint the highest value, allowing the code to create a new midpoint in the next loop
+            high = mid
+# returning the high and low variable into integers to use in the earlier equation for the next loop
+        low = int(low)
+        high = int(high)
 #creating the GUI
 class PasswordTester:
 #setting up the GUI
@@ -13,39 +59,32 @@ class PasswordTester:
         self.master = master
 #labelling the Title
         master.title("Password Tester")
-
 #creating label for the first password input
         self.inputText1 = Label(master, text= "              Enter Password   ", )
 #position of label on GUI
         self.inputText1.grid(row = 1, column = 0)
-
 #creating entry for the first password
         self.pass1 = StringVar()
         self.enterPass1 = Entry(master, show="*", textvariable=self.pass1)
 #position of entry space
         self.enterPass1.grid(row = 1, column = 1)
-
 #creating label for the second password input
         self.inputText2 = Label(master, text="           Re-Enter Password      ")
 #position of label on GUI
         self.inputText2.grid(row = 2, column = 0)
-
 #creating entry for the second password input
         self.pass2 = StringVar()
         self.enterPass2 = Entry(master, show="*", textvariable=self.pass2)
 #position of entry space
         self.enterPass2.grid(row = 2, column = 1)
-
 #creating button for testing password
         self.TestButton = Button(master, text="Test", command=self.Test)
 #position of button
         self.TestButton.grid(row=4, column=0)
-
 #creating button for exiting code
         self.ExitButton = Button(master, text="Exit", command=master.quit)
 #position of button
         self.ExitButton.grid(row=4, column=1)
-
 #creating space within the GUI or the error messages and results
         self.spaceText = Label(master, text='''                          
         
@@ -53,7 +92,6 @@ class PasswordTester:
         ''')
 #position of created space
         self.spaceText.grid(row = 5, column = 0)
-
 #the function which happens when the user presses the test button
     def Test(self):
 #giving the first password input a variable
@@ -68,7 +106,7 @@ class PasswordTester:
             self.errorText = Label(text= '''Your password must 
     be greater than or equal    
     to 8 characters in length    ''')
-#positoin of error message
+#position of error message
             self.errorText.grid(row=5)
 #if the two entered passwords do not match it informs the user with an error message
         elif p1 != p2:
@@ -96,50 +134,52 @@ class PasswordTester:
             p1 = 0
             p2 = 0
 #splitting the hash into affixes and making them all capitals
+            print(hashPassword)
             hPrefix = hashPassword[0:5]
-            hSuffix = hashPassword[6:]
+            hSuffix = hashPassword[5:]
             hPrefix = hPrefix.upper()
             hSuffix = hSuffix.upper()
-
 #sending the hash prefix to the website and sending the results to a file
             os.system('curl https://api.pwnedpasswords.com/range/' + hPrefix + '>PPR.txt')
-
 #opening the file the send the results from the Pwned passwords website
             PPR = open('PPR.txt', 'r')
-#defining a variable for the while loop
-            x = 0
 #reading the file line by line
-            fileRead = PPR.readline().strip()
+            fileRead = PPR.readline().strip('\n')
+#defining two lists which split the hash suffix and the number of times the password has been Pwned from the file
+            fileList = []
+            pwnedList = []
 #for each line within the file
             while fileRead:
-#search for the hash suffix within the line
-                fileSearch = fileRead.find(hSuffix)
-#if the hash suffix is found within the line
-                if fileSearch != -1:
-                    result = fileRead[36:]
-# creating the result message
-                    self.resultText = Label(text='''this password 
-      has been PWNED 
+#add the hash prefix and number of times the password has been Pwned to their corresponding lists
+                fileList.append(fileRead[:35])
+                pwnedList.append(fileRead[36:])
+#reading the next line for the loop
+                fileRead = PPR.readline().strip('\n')
+#using the binary search to get a return on whether or not the password has been Pwned
+            position = BinarySearch(fileList, hSuffix)
+#position[0] is the position in the list of hash suffixes and the result is that same position on the Pwned list
+            result = pwnedList[int(position[0])]
+#position[1] is the binary value of whether or not the hash suffix has been found in the list
+            Pwned = position[1]
+#if the hash suffix has been found:
+            if Pwned == 1:
+#displays a message informing the user that the password they have input is not secure
+                self.resultText = Label(text='''this password
+      has been PWNED
      ''' + result + " times     ")
 # position of result message
-                    self.resultText.grid(row=5, column=1)
-#making x = 1 for if the suffix has been found
-                    x = 1
-#reading the next line and repeat process
-                fileRead = PPR.readline().strip()
-#if the suffix has not been found
-            if x == 0:
-# creating the result message
-                self.resultText = Label(text='''  this password 
-has not been 
+                self.resultText.grid(row=5, column=1)
+#if the hash suffix has NOT been found:
+            else:
+#displays a message saying the password has not been Pwned
+                self.resultText = Label(text='''  this password
+has not been
       PWNED               ''')
-
 # position of result message
                 self.resultText.grid(row=5, column=1)
-
+            Pwned = 0
 #deleting the file with all the possible hash suffixes
 os.system('rm PPR.txt')
-
 #Functions to make the GUI work
 root = Tk()
 gui = PasswordTester(root)
